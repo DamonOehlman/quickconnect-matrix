7ccf1cf55b13d9511dd8eadcf7795cdcc7946f27
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-26 15:34:52 +1100

    Extend the timeout for the 4 x matrix

--- test/matrix-4.js
+++ test/matrix-4.js
@@ -4,7 +4,7 @@ var connections;
 
 test('create a connection matrix of 4 connections', function(t) {
   t.plan(1);
-  connections = matrix({ count: 4, timeout: 20e3 }, function(err) {
+  connections = matrix({ count: 4, timeout: 40e3 }, function(err) {
     t.ifError(err);
   });
 });
